2b6e780e41bf6121cfd785053d92218e1d3bee29
Edder <enaranjo5489@uta.edu.ec>
2024-06-25 22:57:36 -0500

    Creacion Vista Usuario

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,70 @@
+// pages/user/index.tsx
+import { useAuth } from "../../context/AuthContext";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Cookies from "js-cookie";
+import Navbar from "../../components/user/Navbar"; 
+
+const UserPage = () => {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user && user.role && user.role !== "user") {
+      router.push("/login");
+    }
+  }, [user, router]);
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+    router.push("/login");
+  };
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Navbar/>
+      <div className="flex flex-col items-center justify-center bg-white flex-grow">
+      <h1 className="text-4xl font-bold text-purple-700 mb-10">
+          Welcome User - Menu
+        </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-4xl">
+
+        <Link href="/user/">
+          <div className="bg-gradient-to-r from-blue-400 to-purple-500 border border-gray-300 rounded-lg p-6 text-center text-white transition-transform transform hover:scale-105">
+            Manage Users
+          </div>
+        </Link>
+        <Link href="/admin/assign-roles">
+          <div className="bg-gradient-to-r from-blue-400 to-purple-500 border border-gray-300 rounded-lg p-6 text-center text-white transition-transform transform hover:scale-105">
+            Assign Roles
+          </div>
+        </Link>
+        <Link href="/admin/manage-servers">
+          <div className="bg-gradient-to-r from-blue-400 to-purple-500 border border-gray-300 rounded-lg p-6 text-center text-white transition-transform transform hover:scale-105">
+            Manage Servers
+          </div>
+        </Link>
+        <div className="bg-gradient-to-r from-blue-400 to-purple-500 border border-gray-300 rounded-lg p-6 text-center text-white transition-transform transform hover:scale-105">
+          <Link href="/admin/manage-templates">
+          Manage Templates
+          </Link>
+        </div>
+        <Link href="/user/reporter">
+          <div className="bg-gradient-to-r from-blue-400 to-purple-500 border border-gray-300 rounded-lg p-6 text-center text-white transition-transform transform hover:scale-105">
+            Create Reports
+          </div>
+        </Link>
+        <div
+          className="bg-gradient-to-r from-red-500 to-red-700 border border-red-600 rounded-lg p-6 text-center text-white cursor-pointer transition-transform transform hover:scale-105"
+          onClick={handleLogout}
+        >
+          Logout
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default UserPage;
